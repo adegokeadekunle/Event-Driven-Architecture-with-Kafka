2f6ca706937564b022cbac0832113c799fd6b61d
adegokeadekunle2 <adegokeadekunle2@gmail.com>
2022-12-30 06:09:41 +0100

    added stock service

---
+++ stock-service/src/main/java/com/adekunle/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,18 @@
+package com.adekunle.stockservice.kafka;
+
+import com.adekunle.basedomain.dto.OrderEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderConsumer {
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent orderEvent) {
+        log.info("Order event received in stock service {}", orderEvent.toString());
+    }
+}
